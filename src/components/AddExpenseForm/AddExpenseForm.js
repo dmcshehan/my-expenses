86a2c8e3a59a13f738fd4ef50ba217fb5685efba
Shehan Disanayake <dmcshehan@outlook.com>
2019-07-30 08:04:53 +0530

    added antd btn to expense form

--- src/components/AddExpenseForm/AddExpenseForm.js
+++ src/components/AddExpenseForm/AddExpenseForm.js
@@ -2,20 +2,19 @@ import React from "react";
 
 //Material UI componenets
 import Paper from "@material-ui/core/Paper";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import FormControl from "@material-ui/core/FormControl";
+
 import InputAdornment from "@material-ui/core/InputAdornment";
 
 import { makeStyles } from "@material-ui/core/styles";
-import DeleteIcon from "@material-ui/icons/Send";
-import UpdateIcon from "@material-ui/icons/Save";
+
+//antd
+import { Form, Icon, Input, Button } from "antd";
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3, 2)
   },
-  textFields: {
+  Inputs: {
     displa: "block"
   },
   button: {
@@ -40,72 +39,49 @@ const AddExpenseForm = props => {
   return (
     <Paper className={classes.root}>
       <form>
-        <FormControl fullWidth>
-          <TextField
-            label="Reason"
-            variant="outlined"
-            margin="normal"
+        <Form.Item>
+          <Input
+            size="large"
             value={props.values.reason}
             onChange={e => props.change(e, "reason")}
           />
-        </FormControl>
-        <FormControl fullWidth>
-          <TextField
-            label="Cost"
-            variant="outlined"
-            margin="normal"
+        </Form.Item>
+        <Form.Item>
+          <Input
             type="number"
             value={props.values.cost}
             onChange={e => props.change(e, "cost")}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  {props.baseCurrency}
-                </InputAdornment>
-              )
-            }}
           />
-        </FormControl>
-        <FormControl fullWidth>
-          <TextField
-            label="Date"
-            margin="normal"
-            type="date"
-            variant="outlined"
-            defaultValue={date}
-            onChange={e => props.change(e, "date")}
-          />
-        </FormControl>
+        </Form.Item>
+        <Form.Item>
+          <Input type="date" onChange={e => props.change(e, "date")} />
+        </Form.Item>
 
-        <FormControl>
+        <Form.Item>
           {!props.btn ? (
             <Button
-              type="submit"
-              variant="contained"
-              color="primary"
+              htmlType="submit"
+              type="primary"
               className={classes.button}
               onClick={e => {
                 props.submit(e);
               }}
             >
               Add
-              <DeleteIcon className={classes.rightIcon} />
             </Button>
           ) : (
             <Button
-              type="submit"
-              variant="contained"
-              color="primary"
+              htmlType="submit"
+              type="primary"
               className={classes.button}
               onClick={e => {
                 props.update(e);
               }}
             >
               Update
-              <UpdateIcon className={classes.rightIcon} />
             </Button>
           )}
-        </FormControl>
+        </Form.Item>
       </form>
     </Paper>
   );
