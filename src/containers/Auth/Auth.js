e3dc59c659cb5d99da1ac6ba7ffcad0dd2ef8827
Shehan Disanayake <dmcshehan@outlook.com>
2019-08-10 19:32:31 +0530

    switch between login and signup

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,6 +4,15 @@ import { Form, Icon, Input, Button, Checkbox, Card, Row, Col } from "antd";
 import styles from "./auth.module.scss";
 
 class NormalLoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.switchForm = this.switchForm.bind(this);
+  }
+  state = {
+    isLogin: true
+  };
+
   handleSubmit = e => {
     e.preventDefault();
     console.log(this.props.form);
@@ -18,12 +27,16 @@ class NormalLoginForm extends React.Component {
     console.log(username);
   };
 
+  switchForm() {
+    this.setState({ isLogin: !this.state.isLogin });
+  }
+
   render() {
     const { getFieldDecorator } = this.props.form;
 
     return (
       <Row justify="center" align="middle">
-        <Col sm={4} md={6} lg={8} offset={8}>
+        <Col sm={4} md={6} lg={6} offset={9}>
           <Card hoverable>
             <Form onSubmit={this.handleSubmit} className="login-form">
               <Form.Item>
@@ -60,46 +73,54 @@ class NormalLoginForm extends React.Component {
                   valuePropName: "checked",
                   initialValue: true
                 })(<Checkbox>Remember me</Checkbox>)}
-                <Button
-                  type="link"
-                  className="login-form-forgot"
-                  style={{ padding: 0 }}
-                >
-                  Forgot password
-                </Button>
+                {this.state.isLogin ? (
+                  <Button
+                    type="link"
+                    className="login-form-forgot"
+                    style={{ padding: 0 }}
+                  >
+                    Forgot password
+                  </Button>
+                ) : null}
                 <Button
                   block
                   type="primary"
                   htmlType="submit"
                   className="login-form-button"
                 >
-                  Log in
+                  {this.state.isLogin ? "Log in" : "Signup"}
                 </Button>
                 Or{" "}
-                <Button type="link" style={{ padding: 0 }}>
-                  register now!
+                <Button
+                  type="link"
+                  style={{ padding: 0 }}
+                  onClick={this.switchForm}
+                >
+                  {this.state.isLogin ? "register now!" : "signin!"}
                 </Button>
               </Form.Item>
-              <Form.Item>
-                <Button
-                  block
-                  className={`login-form-button ${styles.googleBtn} ${
-                    styles.loginBtn
-                  }`}
-                  icon="google"
-                >
-                  Login with Google
-                </Button>
-                <Button
-                  block
-                  className={`login-form-button ${styles.facebookBtn} ${
-                    styles.loginBtn
-                  }`}
-                  icon="facebook"
-                >
-                  Login with Facebook
-                </Button>
-              </Form.Item>
+              {this.state.isLogin ? (
+                <Form.Item>
+                  <Button
+                    block
+                    className={`login-form-button ${styles.googleBtn} ${
+                      styles.loginBtn
+                    }`}
+                    icon="google"
+                  >
+                    Login with Google
+                  </Button>
+                  <Button
+                    block
+                    className={`login-form-button ${styles.facebookBtn} ${
+                      styles.loginBtn
+                    }`}
+                    icon="facebook"
+                  >
+                    Login with Facebook
+                  </Button>
+                </Form.Item>
+              ) : null}
             </Form>
           </Card>
         </Col>
