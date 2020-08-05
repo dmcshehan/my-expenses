03a656470d35484edaf418a99409f6a8de303788
Shehan <dmcshehan@outlook.com>
2020-08-05 10:17:33 +0530

    add open and close dropdown logic

--- src/App/NavbarAvatar/NavbarAvatar.js
+++ src/App/NavbarAvatar/NavbarAvatar.js
@@ -1,17 +1,29 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 //antd comps
 import { Avatar } from "antd";
+
+//action creators
+import {
+  openDropdown,
+  closeDropdown,
+} from "../../store/actionCreators/dropdown";
 
 //styles
 import { avatar } from "./NavbarAvatar.module.css";
 
 export default function NavbarAvatar() {
+  const dispatch = useDispatch();
   const { photoURL } = useSelector((state) => state.user).user;
+  const { isDropdownOpen } = useSelector((state) => state.dropDown);
 
   function handleAvatarClick() {
-    console.log("object");
+    if (isDropdownOpen) {
+      return dispatch(closeDropdown());
+    }
+
+    dispatch(openDropdown());
   }
   return (
     <Avatar src={photoURL} className={avatar} onClick={handleAvatarClick} />
